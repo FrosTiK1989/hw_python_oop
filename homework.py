d2d208196a57fbf7f21aa6a65b3b26782932d9e8
Денис Маресов <frostik1989@yandex.ru>
2021-12-09 09:30:47 +0300

    redact @def__init__ in class @InfoMessage

--- homework.py
+++ homework.py
@@ -2,15 +2,18 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    def __init__(self, training_type: str, duration: float,
-                distance: float, speed: float,
+    def __init__(self,
+                training_type: str,
+                duration: float,
+                distance: float,
+                speed: float,
                 calories: float) -> None:
             self.training_type = training_type
             self.duration = duration
             self.distance = distance
             self.speed = speed
             self.calories = calories
-            
+
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
                 f'Длительность: {self.duration:.3f} ч.; '
